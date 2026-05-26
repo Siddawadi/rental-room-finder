2f3c8c749c72bf072c1d7f4f4f21410ea9890e77
siddharthadawadi <siddharthadawadi10@gmail.com>
2026-05-26 15:15:16 +0545

    index

--- index.php
+++ index.php
@@ -28,7 +28,7 @@ if (isset($_POST['login'])) {
         if ($user && password_verify($password, $user['password'])) {
             
             $_SESSION['user_id'] = $user['id'];
-            $_SESSION['username'] = $user['username'];
+            $_SESSION['username'] = $user['name'];
             $_SESSION['email'] = $user['email'];
             $_SESSION['role'] = $user['role'];
             $_SESSION['is_paid'] = $user['is_paid']; 
